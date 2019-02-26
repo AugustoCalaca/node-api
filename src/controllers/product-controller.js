c12dc2d04335ce5b09d4aaeba14f97a93214dc4b
Augusto Calaca <augusto@Calaca.Augusto>
2019-02-26 11:34:01 -0300

    add product-controller.js

---
+++ src/controllers/product-controller.js
@@ -0,0 +1,17 @@
+'use strict';
+
+exports.post = function(req, res, next) {
+  res.status(200).send(req.body);
+};
+
+exports.put = function(req, res, next) {
+  const id = req.params.id;
+  res.status(200).send({
+    id: id,
+    item: req.body
+  });
+};
+
+exports.delete = function(req, res, next) {
+  res.status(200).send(req.body);
+}; 
