f3db72fd87c32e4e87c7307ffe78323e77fcdc55
Augusto Calaca <augusto@Calaca.Augusto>
2019-02-26 11:31:47 -0300

    add index-route.js

---
+++ src/routes/index-route.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.status(200).send({
+    title: 'node api',
+    version: '0.0.2'
+  });
+});
+
+module.exports = router;
