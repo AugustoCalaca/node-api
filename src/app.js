bdf76c3627fea06ee87055d908ba06b939917254
Augusto Calaca <augusto@Calaca.Augusto>
2019-03-01 00:04:32 -0300

    modified app.js

--- src/app.js
+++ src/app.js
@@ -2,39 +2,20 @@
 
 const express = require('express');
 const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+const Product = require('./models/product');
+const indexRoute = require('./routes/index-route'); // load routes
+const productRoute = require('./routes/product-route'); // load product routes
 
 const app = express();
 const router = express.Router();
 
+mongoose.connect('mongodb+srv://augustocalaca:136953mongodb!@node-test-kipz3.mongodb.net/test');
+
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 
-const route = router.get('/', function(req, res, next) {
-  res.status(200).send({
-    title: 'node api',
-    version: '0.0.1'
-  });
-});
-
-const create = router.post('/', function(req, res, next) {
-  res.status(201).send(req.body); // body of request
-});
-
-const put = router.put('/:id', function(req, res, next) {
-  const id = req.params.id;
-  res.status(200).send({
-    id: id,
-    item: req.body
-  });
-});
-
-const del = router.delete('/', function(req, res, next) {
-  res.status(200).send(req.body);
-});
-
-app.use('/', route);
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', del);
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 
 module.exports = app;
